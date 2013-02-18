fe9830030a43d76be642aa89491435b386be26d3
Leeran Raphaely <leeran.raphaely@gmail.com>
2013-02-18 10:13:07 -0800

    Added blurring calculations
    Added blurring calculations to motion_averager, but the program lost
    significant FPS.

--- OpenCVTests/jni/motion_averager.cpp
+++ OpenCVTests/jni/motion_averager.cpp
@@ -17,6 +17,10 @@ extern "C" {
 
     const double EPSILON = 0.00001;
     
+    const int KERNEL_SIZE       = 3;
+    const int KERNEL_SIZE2      = KERNEL_SIZE * KERNEL_SIZE;
+    const int HALF_KERNEL_SIZE  = KERNEL_SIZE / 2;
+    
 JNIEXPORT jobject JNICALL Java_edu_washington_cs_opencvtests_MovementDetection_DetectMovementPosition(JNIEnv* env, jobject,
                                                                                                       jlong currentFrameAddr,
                                                                                                       jlong previousFrameAddr);
@@ -28,19 +32,46 @@ JNIEXPORT jobject JNICALL Java_edu_washington_cs_opencvtests_MovementDetection_D
     Mat& currentFrame  = *(Mat*)currentFrameAddr;
     Mat& previousFrame  = *(Mat*)previousFrameAddr;
     
-    //absdiff(currentFrame, previousFrame, outputFrame);
-    
-    //blur(outputFrame, outputFrame, Point(5, 5));
-    
     Point2d avg(-1.0, -1.0);
     double pointsCounted = 0.0;
+    
+    // In order to blur, we keep track of the sum of the current KERNEL_SIZE X KERNEL_SIZE area around the the pixel,
+    // plus a queue of the sum of columns so that we can subtract the last column out and add the next column in every
+    // x pixel.
+    
+    int columnSumQueue[KERNEL_SIZE];
+    int currQueueIndex;
+    int runningSum;
 
-    for(int y = 2; y < currentFrame.rows - 2; y++)
+    for(int y = HALF_KERNEL_SIZE; y < currentFrame.rows - HALF_KERNEL_SIZE; y++)
     {
-        for(int x = 2; x < currentFrame.cols - 2; x++) {
-            int currPixel = abs(ELEM(currentFrame, x, y) - ELEM(previousFrame, x, y));
+        // calculate the area around the current y for blurring purposes
+        runningSum = 0;
+        for(int i = 0; i < KERNEL_SIZE-1; i++) {
+            columnSumQueue[i] = 0;
+            for(int j = 0; j < KERNEL_SIZE; j++) {
+                int ly = y+j-HALF_KERNEL_SIZE;
+                columnSumQueue[i] += abs(ELEM(currentFrame, i, ly) - ELEM(previousFrame, i, ly));
+            }
+            runningSum += columnSumQueue[i];
+        }
+        columnSumQueue[KERNEL_SIZE-1] = 0;
+        currQueueIndex = KERNEL_SIZE-2;
+        
+        for(int x = HALF_KERNEL_SIZE; x < currentFrame.cols - HALF_KERNEL_SIZE; x++) {
+            currQueueIndex = (currQueueIndex + 1) % KERNEL_SIZE;
             
-            if(currPixel > 30) {
+            runningSum -= columnSumQueue[currQueueIndex];
+            columnSumQueue[currQueueIndex] = 0;
+            for(int j = 0; j < KERNEL_SIZE; j++) {
+                int lx = x+HALF_KERNEL_SIZE, ly = y+j-HALF_KERNEL_SIZE;
+                columnSumQueue[currQueueIndex] += abs(ELEM(currentFrame, lx, ly) - ELEM(previousFrame, lx, ly));
+            }
+            runningSum += columnSumQueue[currQueueIndex];
+            
+            int currPixel = runningSum / KERNEL_SIZE2;
+            
+            if(currPixel > 50) {
                 avg.x = (avg.x * pointsCounted + (double)x) / (pointsCounted + 1.0);
                 avg.y = (avg.y * pointsCounted + (double)y) / (pointsCounted + 1.0);
                 
